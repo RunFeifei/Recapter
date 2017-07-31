c5fa16fc243afd03a93f1a05c14e6ef89808fb6f
彭飞飞 <feifei.peng@dianrong.com>
2017-07-31 16:16:30 +0800

    MultiAdapter OnItemClick

--- recater/src/main/java/com/fei/root/recater/adapter/multi/MultiAdapter.java
+++ recater/src/main/java/com/fei/root/recater/adapter/multi/MultiAdapter.java
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.ViewGroup;
 
+import com.fei.root.recater.action.AdapterAction;
+import com.fei.root.recater.listener.AdapterListeners;
 import com.fei.root.recater.viewholder.CommonHolder;
 
 import java.util.List;
@@ -13,10 +15,12 @@ import java.util.List;
  * 暂没有实现上拉下拉
  */
 
-public abstract class MultiAdapter<Data extends ItemModule> extends RecyclerView.Adapter<CommonHolder> {
+public abstract class MultiAdapter<Data extends ItemModule> extends RecyclerView.Adapter<CommonHolder> implements AdapterAction<Data> {
 
     private List<Data> listData;
     private SparseArray<Integer> sparseArray;
+
+    private AdapterListeners.OnItemClick<Data> onItemClick;
 
     public MultiAdapter(List<Data> listData) {
         this.listData = listData;
@@ -33,10 +37,12 @@ public abstract class MultiAdapter<Data extends ItemModule> extends RecyclerView
         if (size(listData) <= position) {
             return;
         }
-        Data data = listData.get(position);
-        if (data == null) {
-            throw new IllegalStateException("wrapper is null");
+        int layoutPosition = holder.getLayoutPosition();
+        Data data = getItemData(position);
+        if (onItemClick != null) {
+            holder.itemView.setOnClickListener(view -> onItemClick.onItemClick(data, view, layoutPosition));
         }
+
         if (data instanceof ItemWrapper) {
             ItemWrapper wrapper = (ItemWrapper) data;
             Object object = wrapper.getContent();
@@ -110,5 +116,64 @@ public abstract class MultiAdapter<Data extends ItemModule> extends RecyclerView
 
     }
 
+    @Override
+    public void clearAll(boolean isNotify) {
+        //// TODO: 17-7-31
 
+    }
+
+    @Override
+    public void appendItem(Data data) {
+        //// TODO: 17-7-31
+    }
+
+    @Override
+    public void appendItems(List<Data> datas) {
+        //// TODO: 17-7-31
+    }
+
+    @Override
+    public void insertItem(int position, Data data) {
+        //// TODO: 17-7-31
+    }
+
+    @Override
+    public void removeItem(int position) {
+        //// TODO: 17-7-31
+    }
+
+    @Override
+    public void updateItem(int positon, Data data) {
+        //// TODO: 17-7-31
+    }
+
+    @Override
+    public void setOnItemClick(AdapterListeners.OnItemClick<Data> onItemClick) {
+        this.onItemClick=onItemClick;
+    }
+
+    @Override
+    public void setOnItemLongClick(AdapterListeners.OnItemLongClick<Data> onItemLongClick) {
+        //// TODO: 17-7-31
+    }
+
+    @Override
+    public RecyclerView getRecyclerView() {
+        //// TODO: 17-7-31
+        return null;
+    }
+
+    @Override
+    public List<Data> getDataList() {
+        //// TODO: 17-7-31
+        return null;
+    }
+
+    @Override
+    public Data getItemData(int position) {
+        if (size(listData) <= position || position < 0) {
+            return null;
+        }
+        return listData.get(position);
+    }
 }
